748eecbd652a0c2d4743ebf46bf599e603b7d332
dornelasxz <rafaeldornelasalmeida@gmail.com>
2023-09-04 10:15:22 -0300

    addcode

--- 1014.c
+++ 1014.c
@@ -3,8 +3,14 @@
 int main(){
     int x;
     double y;
+    double resultado;
 
-    
+    scanf("%d", &x);
+    scanf("%lf", &y);
+
+    resultado = x/y;
+
+    printf("%.3f km/l\n", resultado);
 
     return 0;
 }
